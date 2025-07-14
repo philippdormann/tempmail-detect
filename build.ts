79d40074f676c161981c49f3cb59f0bb48cbbfab
Philipp Dormann <philipp@philippdormann.de>
2025-07-14 12:20:33 +0200

    fix: drop 1 list, it contained gmail.com and more - not temp mails imo

--- build.ts
+++ build.ts
@@ -6,7 +6,7 @@ const urls = [
   "https://gist.githubusercontent.com/bitbybit/3eea6c4bae7a2b606cf3/raw/8080fe07e494197c195074ea1831ad2167ad9337/temp-email.txt",
   "https://gist.githubusercontent.com/kero3009destiny/5b22c98d6a5688ddc9891dd894540af0/raw/1d2a9ec781ccc204f5f59ba1cb87064ab6a78647/gistfile1.txt",
   "https://raw.githubusercontent.com/disposable/disposable-email-domains/master/domains.txt",
-  "https://gist.githubusercontent.com/ammarshah/f5c2624d767f91a7cbdc4e54db8dd0bf/raw/660fd949eba09c0b86574d9d3aa0f2137161fc7c/all_email_provider_domains.txt",
+  // "https://gist.githubusercontent.com/ammarshah/f5c2624d767f91a7cbdc4e54db8dd0bf/raw/660fd949eba09c0b86574d9d3aa0f2137161fc7c/all_email_provider_domains.txt",
   "https://raw.githubusercontent.com/sajjadh47/disposable-email-domains-list/refs/heads/main/domains.txt"
 ]
 
